cdc8a9fcd1309d9da01a85a41adbd2af168e3981
Simon Kuruc <simonkuruc69@gmail.com>
2024-03-01 11:34:52 +0100

    Dub nakodovane

--- forest/forest/Dub.cs
+++ forest/forest/Dub.cs
@@ -1,1 +1,50 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forest
+{
+ strom dub = new Dub();
+ Dub dub1 = new Dub();
+
+ try
+ {
+   dub1 rastie();
+    DivisionByZeroException e;
+ }
+catch
+{
+   Console.WriteLine(e.Message);
+}
+class VyscholException: Exception
+{
+    VyscholException() {message = Vyschol };
+
+    override string message
+}
+class Dub
+{
+    override public void rastie()
+    {
+        if (vyschol == true)
+        {
+            throw new VyscholException();
+        }
+    }
+}
+
+ 
+
+    
+
+
+
+
+
+
+
+
+
+}
