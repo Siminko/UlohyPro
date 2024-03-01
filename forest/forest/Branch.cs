90426ca9e5d831319c989af9db69dd64e4feb123
Simon Kuruc <simonkuruc69@gmail.com>
2024-03-01 10:55:48 +0100

    Branch skola

--- forest/forest/Branch.cs
+++ forest/forest/Branch.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+namespace Forest 
+{ internal class Branch 
 
-namespace forest
-{
-    internal class Branch
     {
-    }
+        protected double radius, height; 
+        public double Volume 
+        { get 
+            { return Math.PI * Math.Pow(this.radius, 2) * this.height * 1.0 / 3.0; } 
+        } public Branch() 
+
+        { 
+            this.radius = 0; this.height = 0; } 
+        public Branch(double radius, double height) 
+        { 
+            this.radius = radius; this.height = height; } 
+        ~Branch() 
+        { Console.WriteLine("The branch was destroyed."); } 
+    } 
 }
+
